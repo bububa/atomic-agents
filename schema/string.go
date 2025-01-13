d2ce377ab43cac8c367fee40630a437ff402a8ef
Syd Xu <prof.syd.xu@gmail.com>
2025-01-13 14:54:16 +0800

    fix(schema): String schema marshal

--- schema/string.go
+++ schema/string.go
@@ -2,6 +2,15 @@ package schema
 
 type String string
 
+func (s String) MarshalJSON() ([]byte, error) {
+  return []byte(s), nil
+}
+
+func (s *String) UnmarshalJSON(b []byte) error {
+  *s = String(b)
+  return nil
+}
+
 func (s String) Attachement() *Attachement {
 	return nil
 }
